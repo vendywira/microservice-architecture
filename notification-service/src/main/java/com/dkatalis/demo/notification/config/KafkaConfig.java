549992619cdccaf9b6942dac86199d0263e900a2
vendywira <wayan.wiranatha@tech.jago.com>
2022-02-23 06:36:25 +0700

    add zipkin server and kafka config

---
+++ notification-service/src/main/java/com/dkatalis/demo/notification/config/KafkaConfig.java
@@ -0,0 +1,58 @@
+package com.dkatalis.demo.notification.config;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${kafka.bootstrap.address}")
+    private String bootstrapAddress;
+
+    @Value("${kafka.groupId}")
+    private String groupId;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
--- order-service/src/main/java/com/dkatalis/demo/order/controller/HomeController.java
+++ order-service/src/main/java/com/dkatalis/demo/order/controller/HomeController.java
@@ -1,16 +1,40 @@
 package com.dkatalis.demo.order.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class HomeController {
 
+    private static final String TOPICS = "generate.order.c";
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Autowired
+    private Tracer tracer;
+
     @GetMapping("/hello")
     public String home() {
         log.info("#order-service called!");
         return "hello world from order!";
+    }
+
+    @GetMapping("/generate-order-notification/{message}")
+    public boolean generateOrderNotification(@PathVariable String message) {
+        log.info("#generate-order-notification");
+        String traceId = tracer.currentSpan().context().traceId();
+        try {
+            kafkaTemplate.send(TOPICS, traceId, "order-generate: "+ message);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
