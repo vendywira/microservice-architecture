0d31162a44318b0487f9eba6382cae55068364e3
vendywira <wayan.wiranatha@tech.jago.com>
2022-02-22 16:05:18 +0700

    update mongodb docker

---
+++ dev-tools/mongo/config/mongo-init.js
@@ -0,0 +1,19 @@
+print('Start creating database ##########################')
+db = db.getSiblingDB('cart_service_db');
+db.createUser(
+    {
+        user: 'cart-service',
+        pwd:  'p@ssw0rd123',
+        roles: [{role: 'readWrite', db: 'cart_service_db'}],
+    }
+);
+
+db = db.getSiblingDB('product_service_db');
+db.createUser(
+    {
+        user: 'product-service',
+        pwd:  'p@ssw0rd123',
+        roles: [{role: 'readWrite', db: 'product_service_db'}],
+    }
+);
+print('End creating database ##########################')
